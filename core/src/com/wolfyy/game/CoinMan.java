fd0f50b77698f35197dd990ffa9b1e21f41468ed
Aadarsh Agarwal <aadarshagarwal2229@gmail.com>
2020-09-12 15:40:23 +0530

    ADDING COMMENTS

--- core/src/com/wolfyy/game/CoinMan.java
+++ core/src/com/wolfyy/game/CoinMan.java
@@ -9,28 +9,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class CoinMan extends ApplicationAdapter
 {
 	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create ()// For First Time
 	{
 		batch = new SpriteBatch(); // STARTING POINT FOR PUTTING SOMETHING ON THE SCREEN
-		img = new Texture("badlogic.jpg"); // IMAGE
+
 	}
 
 	@Override
 	public void render ()// RUNS UNTIL YOU FINISH THE GAME
 	{
-		Gdx.gl.glClearColor(1, 0, 0, 1); // BACKGROUNG COLOR
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // DISPLAY IMAGE
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
