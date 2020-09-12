1e3522ac5b09b5276d22cded79a6749dfb3620ca
Aadarsh Agarwal <aadarshagarwal2229@gmail.com>
2020-09-12 15:05:46 +0530

    Initial commit

---
+++ android/src/com/wolfyy/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.wolfyy.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.wolfyy.game.CoinMan;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinMan(), config);
+	}
+}
